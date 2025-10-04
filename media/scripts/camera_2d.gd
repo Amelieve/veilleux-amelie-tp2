6311fe5241ee98977eebf6c6aee85b48546c85a8
Amelieve <2496667@cmontmorency.qc.ca>
2025-10-04 00:29:39 -0400

    fond & camera

---
+++ media/scripts/camera_2d.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+
+
+func _ready():
+	# Cherche un TileMap dans la scène principale (le niveau)
+	var tilemap = get_tree().get_current_scene().get_node("TileMap")
+	if tilemap:
+		var used_rect = tilemap.get_used_rect()
+		var cell_size = tilemap.tile_set.tile_size
+
+		var level_rect = Rect2(
+			used_rect.position * cell_size,
+			used_rect.size * cell_size
+		)
+
+		limit_left = int(level_rect.position.x)
+		limit_top = int(level_rect.position.y)
+		limit_right = int(level_rect.position.x + level_rect.size.x)
+		limit_bottom = int(level_rect.position.y + level_rect.size.y)
