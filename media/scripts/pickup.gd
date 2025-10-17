7d2dbff1b41528335f3e0013fbfd9bd01ebb90c1
Amelieve <2496667@cmontmorency.qc.ca>
2025-10-17 00:52:45 -0400

    on recommence :)

--- media/scripts/pickup.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var sound_player = $SoundPlayer
-
-func _on_body_entered(body: Node2D) -> void:
-	if body is PlayerController:
-		GameManager.add_coin()
-		queue_free()        
