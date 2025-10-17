962eff92fb1dbb0d9160c1f3872d4bd16b296ee8
Amelieve <2496667@cmontmorency.qc.ca>
2025-10-16 23:37:14 -0400

    rien fonctionne, je suis tannée

--- media/scripts/area_exit.gd
+++ media/scripts/area_exit.gd
@@ -1,6 +1,21 @@
 extends Area2D
+class_name AreaExit
 
+@export var sprite : Sprite2D
+
+var is_open = false
+
+func _ready():
+	close()
+	
+func open():
+	is_open = true
+	sprite.region_rect.position.x = 21
+	
+func close():
+	is_open = false
+	sprite.region_rect.position.x = -2
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is PlayerController:
+	if  is_open && body is PlayerController:
 		GameManager.next_level()
